23e42b2d951df5e24bf4f5c85b832cc24eb6a303
grabbster <65812727+grabbster@users.noreply.github.com>
2020-06-16 14:37:07 +0200

    Add files via upload

---
+++ scripts/Sound.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioClass
+{
+
+    public void PlayAudio(int clip)
+    {
+        AudioSource audiosource;
+        AudioClip[] geluid;
+        audiosource = Camera.main.transform.Find("EmptyCamera").GetComponent<AudioSource>();
+        //AudioSource audio = GetComponent<AudioSource> ();
+        geluid = new AudioClip[]
+        {
+            (AudioClip)Resources.Load ("geluid/ontplof") as AudioClip,
+            (AudioClip)Resources.Load ("geluid/spacesound") as AudioClip,
+            (AudioClip)Resources.Load ("geluid/snoop") as AudioClip
+        };
+        //audio.PlayOneShot (geluid [clip], 0.7f);
+        audiosource.clip = geluid[clip];
+        audiosource.Play();
+    }
+
+    public void PlayAudio(int clip, string AudioSourceString)
+    {
+        AudioSource audio;
+        AudioClip[] geluid;
+
+        audio = Camera.main.transform.Find(AudioSourceString).GetComponent<AudioSource>();
+        geluid = new AudioClip[]
+        {
+            (AudioClip)Resources.Load ("geluid/ontplof") as AudioClip,
+            (AudioClip)Resources.Load ("geluid/spacesound") as AudioClip,
+            (AudioClip)Resources.Load ("geluid/snoop") as AudioClip
+        };
+        //audio.PlayOneShot (geluid [clip], 0.7f);
+        audio.clip = geluid[clip];
+        audio.Play();
+    }
+
+}
---
+++ scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] groups;
+
+    public void spawnNext() {
+        //random index
+        int i = Random.Range(0, groups.Length);
+        //spawn goup at current position
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+
+	// Use this for initialization
+	void Start () {
+        //start game with block
+        spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
